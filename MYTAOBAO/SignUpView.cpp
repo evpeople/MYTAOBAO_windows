69d0e0ca52510adc7fbce7fb1c1617009731d92a
hangcaihui <wangzhe177@bupt.edu.cn>
2021-05-15 16:56:37 +0800

    开始登陆页面的编辑工作

--- MYTAOBAO/SignUpView.cpp
+++ MYTAOBAO/SignUpView.cpp
@@ -1,1 +1,14 @@
 #include "SignUpView.h"
+
+void SignUpView::show()
+{
+}
+
+void SignUpView::viewInput()
+{
+}
+
+bool SignUpView::dealInput(std::string name, std::string pass, int choice)
+{
+    return false;
+}
--- MYTAOBAO/SignUpView.h
+++ MYTAOBAO/SignUpView.h
@@ -3,10 +3,7 @@
 class SignUpView :
     public BaseView
 {
+    virtual void show()override;
+    virtual void viewInput()override;
+    bool dealInput(std::string name, std::string pass, int choice);
 };
-
-class SignupView :
-    public BaseView
-{
-};
-
